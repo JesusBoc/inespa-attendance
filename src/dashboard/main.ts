b34cf86da9edf90b77f6d9951b2f1fd750b2ef57
Jesus Bocanegra <jesusbocd@gmail.comm>
2026-02-16 14:44:09 -0500

    Modo oscuro predeterminado, algunos estilos refinados

--- src/dashboard/main.ts
+++ src/dashboard/main.ts
@@ -1,12 +1,9 @@
-import { dummyData } from "../mockData"
 import { EstadoAsistencia } from "../model/asistencia"
 import { supabase } from "../supabase"
-import { ReporteAsistenciaEstudiante } from "./domain/reports/ReporteAsistenciaEstudiante"
 import type { AsistenciaDTO } from "./infrastructure/dto/AsistenciaDTO"
 import type { ReporteDTO } from "./infrastructure/dto/ReporteDTO"
 import { ReporteAsistenciaFactory } from "./infrastructure/ReporteAsistenciaFactory"
 import { ReportePorFechaMapper } from "./infrastructure/ReportePorFechaFactory"
-import { dummyPorFechas } from "./testData/DummyPorFecha"
 import { DashboardController } from "./ui/controllers/DashboardController"
 import { DashboardViewModel } from "./ui/viewmodels/DashboardViewModel"
 
@@ -15,67 +12,80 @@ const params = new URLSearchParams(window.location.search)
 const grupo_id = params.get('grupo_id')
 
 async function main() {
-    let reportes: AsistenciaDTO[]
-    let resumen: ReporteDTO[]
+
+    const { data: { user } } = await supabase.auth.getUser()
+    if (!user) {
+        location.replace('index.html')
+        return
+    }
+
     if (!grupo_id) {
         alert("Debe especificar una id de grupo")
-        reportes = dummyPorFechas
-        resumen = dummyData
-        //return
+        return
+    }
+
+    const { data: grupo } = await supabase
+        .from('grupos')
+        .select('*')
+        .eq('id', grupo_id)
+        .single()
+    
+    const subtitle = document.getElementById('dash-subtitle')!
+    subtitle.innerText = `Grupo: ${grupo?.nombre}`
+    let reportes: AsistenciaDTO[]
+    let resumen: ReporteDTO[]
+    const { data: asistencias, error: errorAsistencias } = await supabase
+        .from('asistencias_por_dia')
+        .select('*')
+        .eq('grupo_id', grupo_id)
+
+    if (errorAsistencias) {
+        alert(errorAsistencias.message)
+    }
+
+    if (!asistencias) {
+        reportes = []
     }
     else {
-        const { data: asistencias, error: errorAsistencias } = await supabase
-            .from('asistencias_por_dia')
-            .select('*')
-            .eq('grupo_id', grupo_id)
-
-        if(errorAsistencias){
-            alert(errorAsistencias.message)
-        }
-
-        if (!asistencias) {
-            reportes = dummyPorFechas
-        }
-        else {
-            reportes = asistencias?.map(
-                a => {
-                    return {
-                        nombre: a.nombre ?? '',
-                        apellido: a.apellido ?? '',
-                        estado: a.estado as EstadoAsistencia || EstadoAsistencia.Ausente,
-                        fecha: a.fecha ?? '2026-01-01',
-                        id: a.id ?? '',
-                        materia: a.materia ?? '',
-                    } as AsistenciaDTO
-                }
-            )
-        }
-        const { data: reportesResumidos, error: errorReportes } = await supabase
-            .from('asistencias_resumen')
-            .select('*')
-            .eq('grupo_id', grupo_id)
-        
-        if(errorReportes){
-            alert(errorReportes.message)
-        }
-        if (!reportesResumidos) {
-            resumen = dummyData
-        }
-        else {
-            resumen = reportesResumidos?.map(
-                a => {
-                    return {
-                        id: a.estudiante_id,
-                        nombre: a.nombre,
-                        apellido: a.apellido,
-                        estado: a.estado,
-                        materia: a.materia,
-                        count: a.total
-                    } as ReporteDTO
-                }
-            )
-        }
+        reportes = asistencias?.map(
+            a => {
+                return {
+                    nombre: a.nombre ?? '',
+                    apellido: a.apellido ?? '',
+                    estado: a.estado as EstadoAsistencia || EstadoAsistencia.Ausente,
+                    fecha: a.fecha ?? '2026-01-01',
+                    id: a.id ?? '',
+                    materia: a.materia ?? '',
+                } as AsistenciaDTO
+            }
+        )
     }
+    const { data: reportesResumidos, error: errorReportes } = await supabase
+        .from('asistencias_resumen')
+        .select('*')
+        .eq('grupo_id', grupo_id)
+
+    if (errorReportes) {
+        alert(errorReportes.message)
+    }
+    if (!reportesResumidos) {
+        resumen = []
+    }
+    else {
+        resumen = reportesResumidos?.map(
+            a => {
+                return {
+                    id: a.estudiante_id,
+                    nombre: a.nombre,
+                    apellido: a.apellido,
+                    estado: a.estado,
+                    materia: a.materia,
+                    count: a.total
+                } as ReporteDTO
+            }
+        )
+    }
+
 
     const reporteResumen = ReporteAsistenciaFactory.buildAll(
         resumen
